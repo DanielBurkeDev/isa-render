2f20724b86107e95cd87157f093d0a4d0b26cb7d
DanielBurkeDev <danielburkedev@gmail.com>
2024-02-22 17:47:40 +0000

    testing db

--- backend/settings.py
+++ backend/settings.py
@@ -83,18 +83,18 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
-if "DATABASE_URL" in os.environ:
-    DATABASES["default"] = dj_database_url.config(
-        conn_max_age=500,
-        conn_health_checks=True,
-    )
+# if "DATABASE_URL" in os.environ:
+#     DATABASES["default"] = dj_database_url.config(
+#         conn_max_age=500,
+#         conn_health_checks=True,
+#     )
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
