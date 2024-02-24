3f2d3f6935cc22fe54d2e1811d7e457caa3af8f5
DanielBurkeDev <danielburkedev@gmail.com>
2024-02-24 01:19:25 +0000

    updated allowed domains

--- backend/settings.py
+++ backend/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = [
     "dburke.pythonanywhere.com",
     "127.0.0.1",
     "127.0.0.1:5500",
-    "irishskateparksapp.danielburkedev.com",
+    "https://irishskateparksapp.danielburkedev.com",
 ]
 
 
@@ -127,6 +127,7 @@ CORS_ALLOW_HEADERS = default_headers + ("Access-Control-Allow-Origin",)
 CORS_ORIGIN_WHITELIST = (
     "http://127.0.0.1:5500",
     "http://localhost:5500/",
+    "irishskateparksapp.danielburkedev.com",
 )
 
 CSRF_TRUSTED_ORIGINS = [
